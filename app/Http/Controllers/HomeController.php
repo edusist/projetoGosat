0c2beb19edd1c214e9d726294e12df1761d27bdf
augustoeduribeiro <augustoeduribeiro@gmail.com>
2023-04-20 18:36:21 -0300

    guzzlehtml

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Response;
+use GuzzleHttp\Client;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-      
-        return view('home.index', ['title' => 'home', 'name'=>'Eduardo']);
+    public function index()
+    {   
+  
+        $client  =  new Client([ 'base_uri'  =>  'https://dev.gosat.org/api/v1/simulacao/oferta']);
+                
+        
+     
+        echo  $client ->getStatusCode();
+        // "200"
+        echo  $client ->getHeader('content-type')[0];
+        // 'application/json; charset=utf8'
+        echo  $client ->getBody();
+
+        
+
+
+        // dd($response);
+        // return view('home.index', ['title' => 'home', 'name'=>'Eduardo']);
     }
 }
